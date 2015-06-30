2c1223aa70a72e1e59258e2b4e98cfd41e75b357
Victor Thibert <victor.thibert@gmail.com>
2015-06-30 02:01:48 -0400

    Fixed for basic filtering by playerID & teamID
    Add filter cases

--- php/getusgbyteam.php
+++ php/getusgbyteam.php
@@ -6,6 +6,8 @@
 	$database = "2014-2015";
 	$teamString = (string)$_GET['teamID'];
 	$teamIDs = array_map('intval', explode('$', $teamString));
+	$playerString = (string)$_GET['playerID'];
+	$playerIDs = array_map('intval', explode('$', $playerString));
 
 	$cnx = mysqli_connect($servername, $username, $password, $database);
 
@@ -13,7 +15,15 @@
 	    die("Connection failed: " . mysqli_connect_error());
 	}
 
-	$query = "SELECT `PLAYER_ID`, `PLAYER_NAME`, `USG_PCT` FROM `2014-2015leagueplayerstats` WHERE `TEAM_ID` IN (" . implode(',', $teamIDs) .") ";
+	if ( ! $playerString) {
+		$query = "SELECT `PLAYER_ID`, `PLAYER_NAME`, `USG_PCT` FROM `2014-2015leagueplayerstats` WHERE `TEAM_ID` IN (" . implode(',', $teamIDs) .") ";
+	} else { if ( ! $teamString ) {
+		$query = "SELECT `PLAYER_ID`, `PLAYER_NAME`, `USG_PCT` FROM `2014-2015leagueplayerstats` WHERE `PLAYER_ID` IN (" . implode(',', $playerIDs) .") ";
+		} else {
+			$query = "SELECT `PLAYER_ID`, `PLAYER_NAME`, `USG_PCT` FROM `2014-2015leagueplayerstats` WHERE `PLAYER_ID` IN (" . implode(',', $playerIDs) .") AND `TEAM_ID` IN (" . implode(',', $teamIDs) .") ";
+		}
+
+	}
 
 	if ( ! $query ) {
 		echo mysql_error();
