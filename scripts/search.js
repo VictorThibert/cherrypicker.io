902cb3ad8355fce715dff6500d8bb8fcd4a571a1
Laurence Li <li.laurence55@gmail.com>
2015-07-18 17:40:33 -0400

    Hardcoded array into search.js
    Hardcoded array into search.js

--- scripts/search.js
+++ scripts/search.js
@@ -1,9 +1,39 @@
 var keys;
 
-d3.csv("../data/teams.csv",function (csv) {
-    keys = csv;
-    start();
-});
+keys = [
+          {"Team":"Atlanta Hawks"},
+          {"Team":"Boston Celtics"},
+          {"Team":"Brooklyn Nets"},
+          {"Team":"Charlotte Hornets"},
+          {"Team":"Chicago Bulls"},
+          {"Team":"Cleveland Cavaliers"},
+          {"Team":"Dallas Mavericks"},
+          {"Team":"Denver Nuggets"},
+          {"Team":"Detroit Pistons"},
+          {"Team":"Golden State Warriors"},
+          {"Team":"Houston Rockets"},
+          {"Team":"Indiana Pacers"},
+          {"Team":"Los Angeles Clippers"},
+          {"Team":"Los Angeles Lakers"},
+          {"Team":"Memphis Grizzlies"},
+          {"Team":"Miami Heat"},
+          {"Team":"Milwaukee Bucks"},
+          {"Team":"Minnesota Timberwolves"},
+          {"Team":"New Orleans Pelicans"},
+          {"Team":"New York Knicks"},
+          {"Team":"Oklahoma City Thunder"},
+          {"Team":"Orlando Magic"},
+          {"Team":"Philadelphia 76ers"},
+          {"Team":"Phoenix Suns"},
+          {"Team":"Portland Trailblazers"},
+          {"Team":"Sacramento Kings"},
+          {"Team":"San Antonio Spurs"},
+          {"Team":"Toronto Raptors"},
+          {"Team":"Utah Jazz"},
+          {"Team":"Washington Wizard"}
+        ];
+        
+start();
 
 function onSelect(d) {
     alert(d.Team);
