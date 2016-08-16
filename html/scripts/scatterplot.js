f76b67538e361265bcdeb2ad79946f597eadaab4
VictorThibert <victor.thibert@gmail.com>
2016-08-15 22:40:40 -0400

    ssh test

--- html/scripts/scatterplot.js
+++ html/scripts/scatterplot.js
@@ -1,5 +1,5 @@
 function renderScatterplot() {
-	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	var margin = {top: 20, right: 20, bottom: 30, left: 30},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
