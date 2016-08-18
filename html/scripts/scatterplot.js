3f2f70aa01a84511757e7204eea7b2ef5ab06460
VictorThibert <victor.thibert@gmail.com>
2016-08-17 21:52:44 -0400

    small scatterplot fix

--- html/scripts/scatterplot.js
+++ html/scripts/scatterplot.js
@@ -1,4 +1,4 @@
-function renderScatterplsot() {
+function renderScatterplot() {
 	var margin = {top: 20, right: 20, bottom: 30, left: 30},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
