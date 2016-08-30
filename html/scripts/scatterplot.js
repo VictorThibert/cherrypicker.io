83d3f347bfc42a72936a0d2eb82dfac548af5d85
VictorThibert <victor.thibert@gmail.com>
2016-08-29 22:17:09 -0400

    removed unncessary legend code

--- html/scripts/scatterplot.js
+++ html/scripts/scatterplot.js
@@ -118,26 +118,5 @@ function renderScatterplotInner(data, teamID) {
                .style("opacity", 0);
       });
 
-//   // draw legend
-//   var legend = svg.selectAll(".legend")
-//       .data(color.domain())
-//     .enter().append("g")
-//       .attr("class", "legend")
-//       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-//   // draw legend colored rectangles
-//   legend.append("rect")
-//       .attr("x", width - 18)
-//       .attr("width", 18)
-//       .attr("height", 18)
-//       .style("fill", color);
-
-//   // draw legend text
-//   legend.append("text")
-//       .attr("x", width - 24)
-//       .attr("y", 9)
-//       .attr("dy", ".35em")
-//       .style("text-anchor", "end")
-//       .text(function(d) { return d;})
 
 }
