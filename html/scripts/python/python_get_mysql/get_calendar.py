76b549920b21f4cd8016f43e1d83c80ec737bfd9
VictorThibert <victor.thibert@gmail.com>
2017-02-05 15:30:39 -0500

    proper python to mysql connection set up on server

--- html/scripts/python/python_get_mysql/get_calendar.py
+++ html/scripts/python/python_get_mysql/get_calendar.py
@@ -7,10 +7,10 @@ from mysql.connector import errorcode
 
 try:
     #start connection
-    cnx = mysql.connector.connect(user='all', password='all', host='http://cherrypicker.io/', database="2014-2015")
+    cnx = mysql.connector.connect(user='all', password='all', host='localhost', database="2014-2015")
     cursor = cnx.cursor()
 
-    query = ('SELECT GAME_ID, GAME_DATE_EST, HOME_TEAM_ID, VISITOR_TEAM_ID, HOME_POINTS, AWAY_POINTS, MATCHUP FROM 2014-2015games ')
+    query = ('SELECT GAME_ID, GAME_DATE_EST, HOME_TEAM_ID, VISITOR_TEAM_ID, HOME_POINTS, AWAY_POINTS, MATCHUP FROM `2014-2015games` ')
 
     # sql = ('UPDATE `2014-2015games` SET HOME_POINTS = %s, MATCHUP = %s WHERE HOME_TEAM_ID = %s AND GAME_ID = %s')
     # data = (home_points, matchup, home_team_id, game_id)
