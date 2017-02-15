abe9114480f057364fb53e8519021ca2d66a7c7c
Victor Thibert <victor.thibert@gmail.com>
2017-02-14 21:58:05 -0500

    pymongo connection

--- cherrypicker-flask/app.py
+++ cherrypicker-flask/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_from_directory, redirect
 from blueprints.blueprints import all_blueprints
+from flask.ext.pymongo import PyMongo
 
 #set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='/static')
+mongo = PyMongo(app)
 
 #remaps base url / to /static/index.html
 @app.route('/')
