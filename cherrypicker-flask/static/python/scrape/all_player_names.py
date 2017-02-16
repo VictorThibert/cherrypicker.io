e8c46f91e92a49246e1ab3a7ceb7f21be6a41792
Victor Thibert <victor.thibert@gmail.com>
2017-02-16 17:55:46 -0500

    scrape for all names done

--- cherrypicker-flask/static/python/scrape/all_player_names.py
+++ cherrypicker-flask/static/python/scrape/all_player_names.py
@@ -1,10 +1,9 @@
-# template file to scrape from stats.nba.com
+# scrape all player names and ids into mongo (step 1)
 #
 # see:      github.com/seemethere/nba_py/wiki/stats.nba.com-Endpoint-Documentation for complete endpoint documention
 # format:   stats.nba.com/stats/{endpoint}/?{params}
 # example:  http://stats.nba.com/stats/commonallplayers/?LeagueID=00&Season=2015-16&IsOnlyCurrentSeason=0
 
-import json
 import requests
 import mongo_connect
 
@@ -13,16 +12,17 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)
 
 # parameters for endpoints
 league_id = '00'
-season = '2015-16'
+season = '1900-17' #every single player
 is_only_current_season = '0'
 
-# url 
+# url endpoint
 url = 'http://stats.nba.com/stats/' \
             'commonallplayers/?' \
             'LeagueID=' + league_id + \
             '&Season=' + season + \
-            '&IsOnlyCurrentSeason=' +  is_only_current_season
+            '&IsOnlyCurrentSeason=' + is_only_current_season
 
+print('Scraping all player names and ids from:')
 print(url)
 
 response = requests.get(url, headers=headers)
@@ -32,7 +32,22 @@ data = response.json()
 # players currently refers to the 'players' collection
 players = mongo_connect.db.players
 
-for item in data:
-    print(item)
+for item in data['resultSets'][0]['rowSet']:
+    
+    # item[0] is NBA:PERSON_ID
+    # item[3] is NBA:DISPLAY_FIRST_LAST (e.g. name of player)
+    player_id = int(item[0])
+    player_name = item[2]
+
+    query_result = players.update(
+        # condition: on player id
+        {'player_id':player_id}, 
+        # insert the following document
+        {
+            'player_id':player_id,
+            'player_name':player_name
+
+        },
+        upsert=True)
 
 mongo_connect.client.close()
