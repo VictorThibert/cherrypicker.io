edb8dd91e4073ea1f81b390ce6347a96bd784722
Victor Thibert <victor.thibert@gmail.com>
2017-03-11 18:53:15 -0500

    began work on box score summary scraper

---
+++ cherrypicker-flask/static/python/scrape/box_score_summary.py
@@ -0,0 +1,101 @@
+# scrape all box score (async)
+#
+# see:      github.com/seemethere/nba_py/wiki/stats.nba.com-Endpoint-Documentation for complete endpoint documention
+# format:   stats.nba.com/stats/{endpoint}/?{params}
+# example:  http://stats.nba.com/stats/boxscoresummaryv2/?LeagueID=00&GameID=0021400001
+# game_id:  002{YY}0{GGGG} where YY in 20YY-2017 and GGGG is the game number that year (e.g. 1 to 1231)
+
+import requests
+import mongo_helper
+import asyncio
+import async_helper
+
+# generates list assuming 1230 games
+def generate_game_ids(leading_from_year, leading_to_year):
+    game_id_list = []
+    for current in range(leading_from_year, leading_to_year + 1):
+        leading_year = str(current)[-2:]
+        # following jumble formats each game day to the proper format
+        game_id_list.extend(list(map(lambda x: '002' + leading_year + '0' + str('000' + str(x))[-4:], [x for x in range(1, 1231)])))
+    return game_id_list
+
+url = 'http://stats.nba.com/stats/boxscoresummaryv2/?LeagueID=00&GameID='
+
+
+# temporary test with 2014-2015(1230 games)
+game_id_list = generate_game_ids(2014,2014)
+
+# temporary container variable to extract the result from async request (find a better way to do this)
+memo = [None]
+loop = asyncio.get_event_loop()
+future = asyncio.ensure_future(async_helper.run(game_id_list, url, memo))
+loop.run_until_complete(future)
+# returned_tasks will contain the each json file for each player's http request 
+returned_tasks = memo[0]
+
+
+# games currently refers to the 'games' collection
+games = mongo_helper.db.games
+
+# insert into mongo
+for element in returned_tasks:
+    for item in element['resultSets'][0]['rowSet']:
+
+        player_id = int(item[0])
+        birth_date = item[6]
+        school = item[7]
+        country = item[8]
+        height = item[10]
+        if str(item[11]).isdigit():
+            weight = int(item[11])
+        else:
+            weight = 0
+
+        roster_status = item[15]
+
+        if str(item[16]).isdigit():
+            current_team_id = int(item[16])
+        else:
+            current_team_id = 0
+
+        current_team_name = item[17]
+
+        if str(item[22]).isdigit() and str(item[23]).isdigit():
+            years_active = {'from':int(item[22]), 'to':int(item[23])}
+        else:
+            years_active = {'from':0, 'to':0}
+
+        if str(item[26]).isdigit():
+            draft_year = int(item[26])
+        else:
+            draft_year = 0
+
+        if str(item[27]).isdigit() and str(item[28]).isdigit():
+            draft_position = {'round':int(item[27]), 'number':int(item[28])}
+        else:
+            draft_position = {'round':0, 'number':0}
+    
+        players.update_one(
+            # condition: on player id
+            {'player_id':player_id}, 
+            # insert the following document (using $set to add new fields without deleting existing fields)
+            { "$set":
+                {
+                    'birth_date':birth_date,
+                    'school':school,
+                    'country':country,
+                    'height':height,
+                    'weight':weight,
+                    'roster_status':roster_status,
+                    'current_team_id':current_team_id,
+                    'current_team_name':current_team_name,
+                    'years_active':years_active,
+                    'draft_year':draft_year,
+                    'draft_position':draft_position
+                }
+
+            },
+            # creates a new document if no document matches the criteria
+            upsert=True)
+
+mongo_helper.client.close()
