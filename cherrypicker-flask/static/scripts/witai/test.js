04ce91ac1740e294747802ad3be31c3e2dcc2252
Victor Thibert <victor.thibert@gmail.com>
2017-03-15 23:14:09 -0400

    test landing page temporary

---
+++ cherrypicker-flask/static/scripts/witai/test.js
@@ -0,0 +1,21 @@
+function askQuery(){
+
+	var query_text = document.getElementById('text').value
+	
+	console.log(query_text);
+
+	// http request to 
+	$.getJSON('http://localhost:5000/witai/test/ask_query', {
+		// paramater argument to pass
+		'query': query_text,
+	}, function(data) {
+		console.log(data);
+	});
+}
+
+function isEnter(key){
+	if(event.keyCode == 13) {
+        askQuery();      
+    }
+
+}
