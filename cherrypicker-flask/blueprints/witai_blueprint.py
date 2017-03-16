26499d474e284a33c0dc58d2e76ffd403d90f7ea
Victor Thibert <victor.thibert@gmail.com>
2017-03-15 23:12:10 -0400

    witai test blueprint js -> flask

--- cherrypicker-flask/blueprints/witai_blueprint.py
+++ cherrypicker-flask/blueprints/witai_blueprint.py
@@ -1,7 +1,26 @@
 import flask
 from flask import Blueprint
+from flask import Flask, request, send_from_directory, redirect, jsonify
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+# see: http://flask.pocoo.org/docs/0.12/patterns/jquery/
 
 #url prefix for all witai related functions
 witai_blueprint = Blueprint('witai', __name__, url_prefix='/witai')
+
+# temporary 
+@witai_blueprint.route('/test')
+def test():
+	return redirect('/static/witai_test.html')
+
+@witai_blueprint.route('/test/askQuery')
+def ask_query():
+    query = request.args.get('query', 0, type=str)
+    print(query)
+    response = jsonify({'a':1, 'b':2})
+    return response
 
 #under add routes
