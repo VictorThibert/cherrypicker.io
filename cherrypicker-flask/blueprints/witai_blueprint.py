5b333c0a923db3333b9776c4a43bc3fa0da40458
Victor Thibert <victor.thibert@gmail.com>
2017-03-16 02:01:20 -0400

    witai connected

--- cherrypicker-flask/blueprints/witai_blueprint.py
+++ cherrypicker-flask/blueprints/witai_blueprint.py
@@ -1,9 +1,18 @@
+import sys
 import flask
 from flask import Blueprint
 from flask import Flask, request, send_from_directory, redirect, jsonify
 from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
+
+# relative path
+# from ..static.witai import query_mongo
+
+# temporary import measure
+sys.path.append('..')
+from static.witai import query_mongo
+sys.path.append('blueprints')
 
 
 # see: http://flask.pocoo.org/docs/0.12/patterns/jquery/
@@ -19,8 +28,7 @@ def test():
 @witai_blueprint.route('/test/ask_query')
 def ask_query():
     query = request.args.get('query', 0, type=str)
-    print(query)
-    response = jsonify({'a':1, 'b':2})
-    return response
+    response = query_mongo.ask(query)
+    return jsonify(response)
 
 #under add routes
