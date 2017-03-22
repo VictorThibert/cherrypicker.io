de846f95a7b1a49e0634e9449e01a3f29bf8c92c
Victor Thibert <victor.thibert@gmail.com>
2017-03-22 02:21:17 -0400

    query infrastructure up to current spec

--- cherrypicker_flask/static/witai/query_mongo.py
+++ cherrypicker_flask/static/witai/query_mongo.py
@@ -1,13 +1,14 @@
 # houses all the querying logic to wit.ai
 
 import sys
-#sys.path.append('../') # temporary measure for ipython --------------
+#sys.path.append('../') # temporary measure for ipython -------------- (and remove .. from python.scrape)
 
 from wit import Wit
 from ..python.scrape import mongo_helper
 from wit.wit import WitError
 import dateutil.parser as parser
 import calendar
+import datetime
 
 # server token: HVAI5NNFRSFNWCDR7U3U4KBDS4FBDM25
 # client token: IRDFALXOU75Q4HBP2TYIDCSVV4LCMTFY
@@ -106,11 +107,8 @@ def parse_date(date_range):
 # run called from flask route
 def ask(query_string):
     
-    # automate this part, hardcoded for testing purposes
-    current_year = 2016
-    current_month = 3
-
     entities = None
+    result_set = {}
 
     try:
         wit_response = client.message(query_string)
@@ -120,6 +118,8 @@ def ask(query_string):
     except WitError:
         return 'witError'
 
+    print('entities', entities)
+
     # format of entities is like
     # {
     #   'NBA_stat':[ {type: , value: , metadata: , confidence: ,}, {}, ...],
@@ -127,9 +127,6 @@ def ask(query_string):
     #   'characteristic':[ {}, {}, ...],
     #   ...
     # }
-
-    print('entities', entities)
-
     # types of queries so far
     # characteristic: weight, age, height, etc.
     # nba_stat and date range (points, assists, rebounds)
@@ -137,52 +134,53 @@ def ask(query_string):
     #   averages
     #   games played
 
-    player_list = all_entities(entities, 'NBA_player', 'metadata')
-    player_1 = player_list[0]
-    print(player_list)
-    nba_stat = first_entity_value(entities, 'NBA_stat')
-    characteristic = first_entity_value(entities,'characteristic')
+    player_all = all_entities(entities, 'NBA_player', 'metadata')
+    nba_stat_all = all_entities(entities, 'NBA_stat', 'value')
+    characteristic_all = all_entities(entities,'characteristic')
+    date_range_all = all_entities(entities, 'date_range')   
+    query_dates = parse_date(str((datetime.datetime.now()).year))
 
-    if characteristic == 'age':
-        characteristic = 'birth_date'
+    if date_range_all is not None:
+        date_range_1 = date_range_all[0]
+        query_dates = parse_date(date_range_1)
 
-    
-    if not player_1:
-        return 'no player selected'
-
-    # not yet suited for multiple dates ranges (only works for first date_range argument)
-    date_range_1 = first_entity_value(entities, 'date_range')
-    query_dates = parse_date(date_range_1)
-
-    # [0] needed to retrieve form cursor
-    result = list(players.find({'player_id':int(player_1)}))[0]
-    
-    game_log = result['game_log']
-    query_games = []
-    sum_of_stat = 0
-
-    print(query_dates)
-    for game in game_log:
-        date = game['game_date']
-        game_id = game['game_id']
+    if player_all is not None:
+        player_1 = player_all[0]
+        player_data = list(players.find({'player_id':int(player_1)}))[0]
         
-        if date >= query_dates[0] and date <= query_dates[1]:
-            query_games.append(game_id)
+        if characteristic_all is not None: # query about characteristic (weight, age, height)
+            characteristic = characteristic_all[0]
+            if characteristic == 'age':
+                characteristic = 'birth_date'
+            result_set[characteristic] = player_data[characteristic]
 
-    box_scores = list(games.find({'game_id':{'$in':query_games}},{'box_score':1}))
+        if nba_stat_all is not None: # query about a stat
 
-    for box_score in box_scores:
-        box_score = box_score['box_score']
-        for player_stat_line in box_score:
-            if str(player_stat_line['player_id']) == str(player_1):
-                sum_of_stat += player_stat_line[nba_stat]
+            nba_stat = nba_stat_all[0]
+
+            game_log = player_data['game_log']
+            query_games = []
+            sum_of_stat = 0
+            count = 0
+
+            for game in game_log:
+                date = game['game_date']
+                game_id = game['game_id']
+                
+                if date >= query_dates[0] and date <= query_dates[1]:
+                    query_games.append(game_id)
+
+            box_scores = list(games.find({'box_score.player_id':int(player_1),'game_id':{'$in':query_games}},{'box_score.$':1, '_id':0}))
+
+            for box_score in box_scores:
+                box_score = box_score['box_score'][0]
+                sum_of_stat += box_score[nba_stat]
+                count += 1
+        
+            result_set[nba_stat] = sum_of_stat
+            result_set['count'] = count
 
 
-    if characteristic:
-        result = result[characteristic]
-
-    # temporary, change
-    result = sum_of_stat
-    return result
+    return str(result_set)
 
 mongo_helper.client.close()
