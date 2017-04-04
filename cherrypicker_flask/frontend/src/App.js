1ddfa0434111ff9297e1a48f0fd6a0334c5f6533
Victor Thibert <victor.thibert@gmail.com>
2017-04-04 01:08:22 -0400

    routing proper began

--- cherrypicker_flask/frontend/src/App.js
+++ cherrypicker_flask/frontend/src/App.js
@@ -1,20 +1,90 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          This is some random text
-        </p>
+      <div className="container-card" id="test">
+        <select onChange={(vv) => console.log(vv.target.value)}>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </select>
       </div>
     );
   }
 }
 
-export default App;
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <Route path="/:id" component={Child}/>
+      </ul>
+
+      <hr/>
+
+      <Route path="/*" exact component={App}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+
+      
+    </div>
+  </Router>
+
+   
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Child = ({ match }) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+// export default App;
+export default BasicExample;
--- cherrypicker_flask/frontend/src/index.js
+++ cherrypicker_flask/frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Appexport from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Appexport />,
   document.getElementById('root')
 );
